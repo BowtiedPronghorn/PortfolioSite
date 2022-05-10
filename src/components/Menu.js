9af61cf5fdbe2ca93984788c07d144174721f89e
BowtiedPronghorn <bowtiedpronghorn@gmail.com>
2022-05-10 13:50:26 +0200

    Added menu bar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,18 +5,28 @@ import { Pivot as Hamburger } from 'hamburger-react'
 
 
 const Menu = () => {
-    const [menuComponent, setMenu] = useState("")
+    const [menuComponent, setMenu] = useState(<div></div>)
     return (
             <div>
-            <Hamburger rounded size={48} color={'white'} distance={'lg'} onToggle={toggled => {
+                {menuComponent}
+            <Hamburger rounded size={48} color={'#36454F'} distance={'lg'} onToggle={toggled => {
                 if (toggled) {
-                    setMenu('Weenis')
+                    setMenu(<div class={"Menubar"}>
+                        <div class={"Divider"}></div>
+                        <ul>
+                            <li><a href={'/'}>Home</a></li>
+                            <li><a href={'/'}>About</a></li>
+                            <li><a href={'/'}>Experience</a></li>
+                            <li><a href={'/'}>Portfolio</a></li>
+                            <li><a href={'/'}>Intersts</a></li>
+                            <li><a href={'/'}>Socials</a></li>
+                        </ul>
+                    </div>)
                 }
                 else {
-                    setMenu("")
+                    setMenu(<div class={"Nobar"}></div>)
                 }
             }} />
-                <h1>{menuComponent}</h1>
             </div>
     );
 };
